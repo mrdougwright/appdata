c903bb09fae4f7e351841c1f59f9d26b8ed0f49d
Doug Wright <mrdougwright@gmail.com>
2013-10-27 09:33:24 -0700

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,30 +16,17 @@ set :keep_releases, 5
 # default_run_options[:pty] = true
 server "162.243.137.33", :app, :web, :db, primary: true
 
-# # namespace :deploy do
-# #   task :start do ; end
-# #   task :stop do ; end
+# for linking application.yml file for Figaro ~ database.yml
+desc "Symlink shared config files"
+task :symlink_config_files do
+  run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/application.yml #{ current_path }/config/application.yml"
+end 
 
-# #   desc "Symlink shared config files"
-# #   task :symlink_config_files do
-# #     run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
-# #   end
+desc "Restart Passenger app"
+task :restart do
+  run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+end  
 
-# #   # NOTE: I don't use this anymore, but this is how I used to do it.
-# #   desc "Precompile assets after deploy"
-# #   task :precompile_assets do
-# #     run <<-CMD
-# #       cd #{ current_path } &&
-# #       #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
-# #     CMD
-# #   end
-
-# #   desc "Restart applicaiton"
-# #   task :restart do
-# #     run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
-# #   end
-# # end
-
-# # after "deploy", "deploy:symlink_config_files"
-# # after "deploy", "deploy:restart"
-# # after "deploy", "deploy:cleanup"
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
