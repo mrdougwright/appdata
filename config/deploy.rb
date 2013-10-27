db16a6aaf5109c58654c24c65748d3c349bd03c0
Doug Wright <mrdougwright@gmail.com>
2013-10-27 09:20:59 -0700

    Don't know what i'm doing

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,44 +2,44 @@ require "bundler/capistrano"
 
 set :application, "appdata"
 set :repository,  "git@github.com:mrdougwright/appdata.git"
-set :deploy_to, "/appdata" # could comment out
+set :deploy_to, "/var/www/#{application}" # could comment out
 set :scm, :git
-# set :scm_passphrase, "password"
+set :scm_passphrase, ""
 set :branch, "master"
 set :user, "doug"
 set :use_sudo, false
 set :rails_env, "production"
 set :deploy_via, :copy
-set :ssh_options, { :forward_agent => true, :port => 4321 }
+set :ssh_options, { :forward_agent => true, :port => 22 }
 set :keep_releases, 5
 
-default_run_options[:pty] = true
+# default_run_options[:pty] = true
 server "162.243.137.33", :app, :web, :db, primary: true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
+# # namespace :deploy do
+# #   task :start do ; end
+# #   task :stop do ; end
 
-#   desc "Symlink shared config files"
-#   task :symlink_config_files do
-#     run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
-#   end
+# #   desc "Symlink shared config files"
+# #   task :symlink_config_files do
+# #     run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+# #   end
 
-#   # NOTE: I don't use this anymore, but this is how I used to do it.
-#   desc "Precompile assets after deploy"
-#   task :precompile_assets do
-#     run <<-CMD
-#       cd #{ current_path } &&
-#       #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
-#     CMD
-#   end
+# #   # NOTE: I don't use this anymore, but this is how I used to do it.
+# #   desc "Precompile assets after deploy"
+# #   task :precompile_assets do
+# #     run <<-CMD
+# #       cd #{ current_path } &&
+# #       #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+# #     CMD
+# #   end
 
-#   desc "Restart applicaiton"
-#   task :restart do
-#     run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
-#   end
-# end
+# #   desc "Restart applicaiton"
+# #   task :restart do
+# #     run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+# #   end
+# # end
 
-# after "deploy", "deploy:symlink_config_files"
-# after "deploy", "deploy:restart"
-# after "deploy", "deploy:cleanup"
+# # after "deploy", "deploy:symlink_config_files"
+# # after "deploy", "deploy:restart"
+# # after "deploy", "deploy:cleanup"
